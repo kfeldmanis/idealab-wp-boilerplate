4edf71eb4a39b12458a2243a0cb7a2d3487931ab
Artūrs Cirsis <arturs.cirsis@gmail.com>
2014-08-22 16:45:46 +0300

    options-callout

--- functions.php
+++ functions.php
@@ -1,9 +1,13 @@
 <?php
 
-  register_nav_menus( array(
-    'header' => 'Header nav',
-    'footer' => 'Footer nav'
-  ));
+
+  // Options Framework (https://github.com/devinsays/options-framework-plugin)
+  if ( !function_exists( 'optionsframework_init' ) ) {
+    define( 'OPTIONS_FRAMEWORK_DIRECTORY', get_template_directory_uri() . '/_inc/' );
+    require_once dirname( __FILE__ ) . '/_inc/options-framework.php';
+  }
+
+  register_nav_menus();
 
   function idea_scripts() {
     wp_enqueue_style( 'theme', get_template_directory_uri() . '/assets/css/theme.css', '1.0.0', true );
